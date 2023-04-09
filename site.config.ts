5b87f73553720c13aaa340f185dd2d89489d897d
bailylu <104406397+bailylu@users.noreply.github.com>
2023-04-09 19:57:52 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: '贝利说',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: '贝利说说',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
